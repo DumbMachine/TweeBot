71f30b7f3cf02addf0c5216c7399db95fb89662c
ASHLEY WARREN <ratin.kumar.2k@gmail.com>
2018-12-29 00:53:59 +0530

    plotting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import tweepy
 from textblob import TextBlob
 import matplotlib.pyplot as plt
+from nltk.corpus import stopwords
 consumer_key =  '2XZ8WwA60fjcn30nUwMcZGNoZ'
 consumer_secret = 'dlWKue8WH17vPRJO3bUJb9w4I1KJHQaD6MV7W0eTJFmeKQ6Gcm'
 access_token = '753985003328012288-tPRbv9Za68YBLdelJEznkud2vJSkxQj'
@@ -19,12 +20,27 @@ for page in tweepy.Cursor(api.search , q = "batman" , count = 10).pages(1):
 #To get a USERS tweets
 x=[]
 y=[]
-for page in tweepy.Cursor(api.user_timeline , id ="Trump" , count = 200).pages(5):
+for page in tweepy.Cursor(api.user_timeline , id ="tommyxtopher" , count = 200).pages(100):
     for tweet in page:
-        x.append(TextBlob(tweet.text).sentiment.polarity)
-        y.append(TextBlob(tweet.text).sentiment.subjectivity)
+        q1 = [word for word in tweet.text.split() if word not in stopwords.words('english')]
+        sum=0
+        for word in q1:
+                sum+= TextBlob(word).sentiment.polarity
+        x.append(sum/len(q1))
+
+
 
 plt.plot(x,color="#9966ff")
+plt.ylim(-1,1,0.005)
 plt.rcParams['axes.facecolor'] = "#000000"
-plt.rcParams['lines.linewidth'] = 5
+plt.rcParams['lines.linewidth'] = 1
 plt.show()
+sum=0
+for nos in x:
+        sum+=nos
+print(sum/len(x))
+
+plt.scatter([i for i  in range(len(x))],x)
+plt.show()
+
+
